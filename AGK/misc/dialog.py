238b9daacd64c655bffa2a7f1ff33964baebcbbf
Kyle Cunningham <klcunningham6@gmail.com>
2017-07-11 15:21:55 +0100

    Adds popup sounds to dialogs.

--- AGK/misc/dialog.py
+++ AGK/misc/dialog.py
@@ -1,14 +1,20 @@
 from ..speech import auto, SAPI
 from ..mainframe import keyboard
+from ..audio import sound
 import pygame
 
 class dialog(object):
-	def __init__(self, text, SAPI=False):
+	def __init__(self, text, popup_sound="", SAPI=False):
 		self.text = text
+		self.popup_sound = popup_sound
 		self.SAPI=SAPI
 		self.display()
 
 	def display(self):
+		if self.popup_sound != "":
+			handle = sound.sound()
+			handle.load(self.popup_sound)
+			handle.play()
 		self.speak(self.text + " To repeat, press R. To dismiss, press enter.")
 		while 1:
 			for evt in pygame.event.get():
