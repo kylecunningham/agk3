63a726d5805d08ae4ede8b1c1919c2df1b834edc
Kyle Cunningham <klcunningham6@gmail.com>
2017-09-03 12:45:05 +0100

    Fully implements disabled items in menus, updates examples.

--- examples/menu_example.py
+++ examples/menu_example.py
@@ -3,9 +3,9 @@ from AGK.misc import menu
 from AGK.mainframe import window, keyboard
 from AGK.speech import auto
 def mainmenu():
-	m=menu.menu(select_sound="snd/Menu_Select.ogg", move_sound="snd/Menu_Click.ogg")
+	m=menu.menu(select_sound="snd/Menu_Select.ogg", move_sound="snd/Menu_Click.ogg", disabled_text="This option is disabled.")
 	m.add_item_tts("Start game","start")
-	m.add_item_tts("Exit game","exit")
+	m.add_item_tts("Exit game","exit",enabled=False)
 	obj=m.run("Main menu")
 	if obj.name=="start":
 		auto.speak("You selected start.")
